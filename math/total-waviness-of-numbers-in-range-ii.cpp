d657640d8b844595655306795656da7f5fa26af9
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-06-05 13:14:15 +0530

    LC-total-waviness-of-numbers-in-range-ii [Runtime: N/A; Memory: N/A]

--- math/total-waviness-of-numbers-in-range-ii.cpp
+++ math/total-waviness-of-numbers-in-range-ii.cpp
@@ -1,27 +1,57 @@
+using ll = long long;
 class Solution {
+    static inline int waves[570];
+    static inline bool init = []() {
+        int j = 0;
+        for (int i = 0; i < 1000; i++) {
+            int r = i % 10;
+            int m = (i / 10) % 10;
+            int l = (i / 100) % 10;
+            if ((m > max(l, r)) | (m < min(l, r)))
+                waves[j++] = i;
+        }
+        return 0;
+    }();
+
 public:
-    long long totalWaviness(long long num1, long long num2) {
-        int ans = 0;
-        for (int x = num1; x <= num2; x++) {
-            ans += f(x);
-        }
-        return ans;
-    }
-    int f(int x) {
-        int nums[20], m = 0;
-        while (x > 0) {
-            nums[m++] = x % 10;
-            x /= 10;
-        }
-        if (m < 3) {
+    ll totalWaviness(ll A, ll B) { return waveCount(B) - waveCount(A - 1); }
+
+private:
+    ll waveCount(ll num) {
+        if (num < 100)
             return 0;
+        ll res = 0;
+        for (auto& p : waves)
+            res += countWays(num, p);
+        return res;
+    }
+
+    ll countWays(ll num, int pattern) {
+        ll type = pattern < 100;
+        ll count = 0, mult = 1;
+
+        for (int i = 0; i < 16; i++) {
+            if (mult * 100 > num)
+                break;
+
+            ll pre = num / (mult * 1000);
+            ll cur = (num / mult) % 1000;
+            ll suf = num % mult;
+
+            ll ways = 0, edge = 0;
+
+            if (cur > pattern)
+                ways = pre - type + 1;
+            else if (cur == pattern) {
+                ways = max(0LL, pre - type);
+                edge = (pre >= type) * (suf + 1);
+            } else if (cur < pattern)
+                ways = max(0LL, pre - type);
+
+            count += ways * mult + edge;
+            mult *= 10;
         }
-        int s = 0;
-        for (int i = 1; i < m - 1; i++) {
-            if ((nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) || (nums[i] < nums[i - 1] && nums[i] < nums[i + 1])) {
-                s++;
-            }
-        }
-        return s;
+
+        return count;
     }
 };
