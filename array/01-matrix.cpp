0340cc900293c19681e17d2af877490a44e0cd7c
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-06-05 08:41:06 +0530

    LC-01-matrix [Runtime: N/A; Memory: N/A]

--- array/01-matrix.cpp
+++ array/01-matrix.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
@@ -15,10 +15,10 @@ public:
 	                q.push({{i,j}, 0}); 
 	                vis[i][j] = 1; 
 	            }
-	            /*else {
+	            else {
 	                // mark unvisited 
 	                vis[i][j] = 0; 
-	            }*/
+	            }
 	        }
 
     }
@@ -47,4 +47,41 @@ public:
 
     }
 
+};*/
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        int INF = m + n; // A safe maximum possible distance
+        
+        // Pass 1: Check Top and Left neighbors (Top-Left to Bottom-Right sweep)
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) continue;
+                
+                int top = INF, left = INF;
+                if (i - 1 >= 0) top = mat[i - 1][j];
+                if (j - 1 >= 0) left = mat[i][j - 1];
+                
+                mat[i][j] = min(top, left) + 1;
+            }
+        }
+        
+        // Pass 2: Check Bottom and Right neighbors (Bottom-Right to Top-Left sweep)
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (mat[i][j] == 0) continue;
+                
+                int bottom = INF, right = INF;
+                if (i + 1 < m) bottom = mat[i + 1][j];
+                if (j + 1 < n) right = mat[i][j + 1];
+                
+                // Compare the current value (from Pass 1) with the bottom/right options
+                mat[i][j] = min(mat[i][j], min(bottom, right) + 1);
+            }
+        }
+        
+        return mat;
+    }
 };
