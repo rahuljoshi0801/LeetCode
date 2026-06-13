2abc631b9204fbeec75de82c98682fd57d228c37
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-06-13 17:20:09 +0530

    LC-weighted-word-mapping [Runtime: N/A; Memory: N/A]

--- array/weighted-word-mapping.cpp
+++ array/weighted-word-mapping.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
-        string res(words.size(), 0);
-        int i = 0;
-
-        for (auto& word : words) {
-            int s = 0;
-            for (auto& c : word)
-                s += weights[(c & (1 << 5) - 1) - 1];
-            res[i++] = 'z' - (s - ((s * 2521) >> (1 << 4)) * 26);
+        string ans="";
+        for(string word: words){
+            int w=0;
+            for(char ch: word){
+                w += weights[ch-'a'];
+            }
+            ans += (char)((25 - w%26) + 'a');
         }
-
-        return res;
+        return ans;
     }
 };
