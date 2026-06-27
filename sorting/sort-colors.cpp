fcd00ebd2dd07d0a3d2112b450293835298c1cda
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-06-27 12:14:21 +0530

    LC-sort-colors [Runtime: 0 ms; Memory: 11.7 MB]

--- sorting/sort-colors.cpp
+++ sorting/sort-colors.cpp
@@ -3,7 +3,7 @@ public:
     void sortColors(vector<int>& nums) {
         int low =0 , high = nums.size()-1;
         int mid = 0;
-        while( mid < high){
+        while( mid <= high){
             if(nums[mid]==0){
                 swap(nums[mid], nums[low]);
                 mid ++;
