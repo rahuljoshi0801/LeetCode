95e34093d366da1007d93cc5f1bd07b3dcd25cf3
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-06-27 21:44:33 +0530

    LC-next-permutation [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/next-permutation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int ind = -1;
+        for( int i = n-2; i < n; i--){
+            if(nums[i]>arr[i+1]){
+            ind = i;
+            break ;
+            }
+        }
+        if( ind = -1){
+            reverse(nums.begin(), nums.end());
+        }
+        for(int i = index; i< n ;i++){
+            if(nums[i]>nums[index]){
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+        reverse(nums.begin() + ind + 1, nums.end());
+        return 
+
+    }
+};
