99a52c387fa45c4987894739ff00ab7817c217e5
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-06-27 13:39:17 +0530

    LC-best-time-to-buy-and-sell-stock [Runtime: N/A; Memory: N/A]

--- array/best-time-to-buy-and-sell-stock.cpp
+++ array/best-time-to-buy-and-sell-stock.cpp
@@ -4,9 +4,8 @@ public:
         int min = prices[0];
         int profit = 0 ; 
         for( int i= 1; i< prices.size(); i++){
-            profit = max( profit , prices[i]-1);
-
-            min = ( min, price[i];)
+            profit = max( profit , prices[i]-min);
+            min = ( min, prices[i]);
         }
         return profit;
 
