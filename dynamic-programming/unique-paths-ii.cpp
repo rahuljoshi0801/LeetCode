e5c7e4c3dea8cf024532651c4e26d459bd0aec91
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-06-30 16:13:42 +0530

    LC-unique-paths-ii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/unique-paths-ii.cpp
+++ dynamic-programming/unique-paths-ii.cpp
@@ -6,8 +6,8 @@ public:
         int n = obstacleGrid[0].size();//column
 
         vector<vector<int>>dp ( n, vector<int>(m,-1));
-        for ( int i =0; i<n;i++){
-            for ( int j =0; j<m;j++){
+        for ( int i =0; i<m;i++){
+            for ( int j =0; j<n;j++){
                 if (obstacleGrid[i][j] == 1) {
                     /* If there's an obstacle at the 
                     cell, no paths can pass through it*/
