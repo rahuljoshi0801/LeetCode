ff27df1bf1d048abec006533ad1e90166bb0bc2a
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-06-30 16:12:11 +0530

    LC-unique-paths-ii [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/unique-paths-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();//row
+
+        int m = obstacleGrid[0].size();//column
+
+        vector<vector<int>>( n, vector<int>(m,-1));
+        for ( int i =0; i<n;i++){
+            for ( int j =0; j<m;j++){
+                if (matrix[i][j] == 1) {
+                    /* If there's an obstacle at the 
+                    cell, no paths can pass through it*/
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (i == 0 && j == 0) {
+                    /* If we are at the starting 
+                    point, there is one path to it*/
+                    dp[i][j] = 1;
+                    continue;
+                }
+                int up = 0;
+                int left = 0;
+                /* Check if we can move up and left 
+                (if not at the edge of the maze)*/
+                if (i > 0)
+                    up = dp[i - 1][j];
+                if (j > 0)
+                    left = dp[i][j - 1];
+
+                /* Total number of paths to reach (i, j)
+                is the sum of paths from above and left*/
+                dp[i][j] = up + left;
+            }
+        }
+        /* The final result is stored in dp[m-1][n-1],
+        which represents the destination*/
+        return dp[m - 1][n - 1];
+
+    }
+};
