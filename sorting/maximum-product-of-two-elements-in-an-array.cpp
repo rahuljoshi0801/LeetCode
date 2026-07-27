9f6d01fff8d919465174b6045b8dba0c592624c8
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-07-27 10:55:19 +0530

    LC-maximum-product-of-two-elements-in-an-array [Runtime: N/A; Memory: N/A]

--- sorting/maximum-product-of-two-elements-in-an-array.cpp
+++ sorting/maximum-product-of-two-elements-in-an-array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        nums.sort();
-        
+        sort(nums.begin(),nums.end());
+
         return (nums[i]-1)*(nums[j]-1);
     }
 };
