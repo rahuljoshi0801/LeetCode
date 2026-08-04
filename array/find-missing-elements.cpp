a6a46c2d4192465b6a23e979d9051fd31177c5b1
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-08-04 17:28:00 +0530

    LC-find-missing-elements [Runtime: N/A; Memory: N/A]

---
+++ array/find-missing-elements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int n = nums.lenth();
+        sort( nums.begin(), nums.end());
+        int j =0;
+        vector<int> ans;
+        for( int i = nums[0]; i<= nums[n-1]; i++){
+            if(i =nums[i]){
+                continue;
+            }
+            ans[j] = nums[i];
+            j++;
+        }
+        return ans;
+    }
+};
