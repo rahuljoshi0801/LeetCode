d6d4620eb36a85dd1f360c1af3138385ade00f34
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-08-05 22:57:20 +0530

    LC-string-to-integer-atoi [Runtime: N/A; Memory: N/A]

--- string/string-to-integer-atoi.cpp
+++ string/string-to-integer-atoi.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int a = s;
-        return a;
+        int n = s.length();
+        int ans =0;
+        for(int i =n-1; i>=0;i--){
+            if(s[i]==" ") continue;
+            if ( s[i] == "-") ans = -1*ans;
+            if ( s[i] == "+") break ;
+            
+
+
+        }
     }
 };
