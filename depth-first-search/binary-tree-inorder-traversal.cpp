174f4dba2f964876df1e9db25e78c7a5a7b07ad4
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-08-08 16:10:59 +0530

    LC-binary-tree-inorder-traversal [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/binary-tree-inorder-traversal.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inOrder(Node* node, vector<int>& res) {
+        if (node == nullptr)
+            return;
+
+        // Traverse the left subtree first
+        inOrder(node->left, res);
+
+        // Visit the current node
+        res.push_back(node->data);
+
+        // Traverse the right subtree last
+        inOrder(node->right, res);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        inOrder(root, res);
+        return res;
+    }
+};
