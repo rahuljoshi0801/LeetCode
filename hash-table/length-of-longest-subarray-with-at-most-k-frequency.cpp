cafaa5d7914c9e1abe20d58a3e2f20df39e2f435
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-08-12 19:13:28 +0530

    LC-length-of-longest-subarray-with-at-most-k-frequency [Runtime: N/A; Memory: N/A]

---
+++ hash-table/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        unordered_map<int ,int > freq ;
+        int res = 0 ;
+        int n = nums.size();
+        int i = 0, j = 0 ;
+        while( j <n){
+            freq[nums[j]]++;
+            while (freq[j]>k) {
+                freq[i]--;
+                i++;
+            }
+            res = max ( res , j-i+1);
+            j++;
+
+        }
+        return res ;
+    }
+};
