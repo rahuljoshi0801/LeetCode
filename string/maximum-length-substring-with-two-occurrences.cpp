37f6ea27a871697748a27c7da45ac4c01c3d1eea
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-08-14 23:45:34 +0530

    LC-maximum-length-substring-with-two-occurrences [Runtime: N/A; Memory: N/A]

--- string/maximum-length-substring-with-two-occurrences.cpp
+++ string/maximum-length-substring-with-two-occurrences.cpp
@@ -7,15 +7,15 @@ public:
         int n = s.length();
         freq[s[i]] = 1;
         while (j < n) {
-
-            if (freq[s[j]] == 3) {
+            freq[s[j]]++;
+            while (freq[s[j]] >2) {
                 freq[s[i]]--;
                 i++;
 
-            } else {
+            } 
                 j++;
-                freq[s[j]]++;
-            }
+                
+            
             len = max(len, j - i + 1);
         }
         return len;
