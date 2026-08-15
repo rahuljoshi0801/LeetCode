75607025aa86f0c7bd48eb617a36c21742634b66
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-08-15 16:05:09 +0530

    LC-longest-subsequence-with-non-zero-bitwise-xor [Runtime: N/A; Memory: N/A]

---
+++ array/longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int longestSubsequence(vector<int>& nums) {
+        
+        int x;
+        std::unordered_set<int> my_set(nums.begin(), nums.end());
+        for (const auto& element : my_set) {
+        x = element^x;
+    }
+    if (x != 0) return my_set.size();
+
+    return my_set.size()-1;
+    }
+};
