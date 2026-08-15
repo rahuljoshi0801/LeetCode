f29c1a1bab4840df74416cd1152f3aa51929aec2
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-08-15 18:20:57 +0530

    LC-3sum-closest [Runtime: N/A; Memory: N/A]

--- sorting/3sum-closest.cpp
+++ sorting/3sum-closest.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        vector<vector<int>> ans;
+
         int n = nums.size();
-        int add = INT_MAX;
+        int add = nums[0] + nums[1] + nums[2];
+
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < n - 2; i++) {
 
-            // Skip duplicate first elements
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-
             int left = i + 1;
             int right = n - 1;
-            
+
             while (left < right) {
+
                 int sum = nums[i] + nums[left] + nums[right];
 
+                // Perfect match
+                if (sum == target) {
+                    return target;
+                }
+
+                // Update closest answer
+                if (abs(sum - target) < abs(add - target)) {
+                    add = sum;
+                }
+
                 if (sum < target) {
-                    add = min(add, mod(sum-target));
                     left++;
                 }
-                else if (sum > target) {
-                    add = min(add, mod(sum-target));
+                else {
                     right--;
                 }
-                else {
-                    return target ; 
-                }
-                
             }
         }
 
-        return sum;
+        return add;
     }
 };
