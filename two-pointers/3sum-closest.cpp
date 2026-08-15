6ea1ad7653d19dd781deab59b144ce08a4957872
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-08-15 18:22:27 +0530

    LC-3sum-closest [Runtime: N/A; Memory: N/A]

--- two-pointers/3sum-closest.cpp
+++ two-pointers/3sum-closest.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-
+        vector<vector<int>> ans;
         int n = nums.size();
-        int add = nums[0] + nums[1] + nums[2];
-
+        int add = INT_MAX;
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < n - 2; i++) {
 
+            // Skip duplicate first elements
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
             int left = i + 1;
             int right = n - 1;
-
+            
             while (left < right) {
-
                 int sum = nums[i] + nums[left] + nums[right];
 
-                // Perfect match
-                if (sum == target) {
-                    return target;
-                }
-
-                // Update closest answer
-                if (abs(sum - target) < abs(add - target)) {
-                    add = sum;
-                }
-
                 if (sum < target) {
+                    //add = min(add, abs(sum-target));
+                    add = ( abs ( add - target) < abs ( sum - target)) ? add : sum;
                     left++;
                 }
-                else {
+                else if (sum > target) {
+                    //add = min(add, abs(sum-target));
+                    add = ( abs ( add - target) < abs ( sum - target)) ? add : sum;
                     right--;
                 }
+                else {
+                    return target ; 
+                }
+                
             }
         }
 
