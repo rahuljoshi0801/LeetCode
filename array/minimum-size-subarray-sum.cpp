f044f2b30a75d747865425fbf634b5c62d991bc4
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-08-22 11:04:26 +0530

    LC-minimum-size-subarray-sum [Runtime: N/A; Memory: N/A]

--- array/minimum-size-subarray-sum.cpp
+++ array/minimum-size-subarray-sum.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-    int minSubArrayLen(int target, vector<int>& nums) {
-        //int i =0 ;j =i+1;
-        int n= nums.size();
-        int count = INT_MAX;
-        for(int i =0 ; i<n;i++){
-            for( int j = i+1;j<n; j++ ){
-                if( nums[ i] + nums[j] >= target){
-                    count = min( count , j -i+1 );
-                    i++ ; j--;
-                    
-                }else if ( nums[ i] + nums[j] <target){
-                    j++;
-                }
+      bool windowfind(int size, vector<int>&nums, int target) {
+        int sum = 0;
+        int i=0;
+        int j=0;
+        int mx=INT_MIN;
+        int n=nums.size();
+        while(j<n){
+            sum+=nums[j];
+            if(j-i+1==size){
+              mx=max(sum,mx);
+              sum-=nums[i];
+              i++;
             }
+            j++;
         }
-        return count == INT_MAX ? 0 : count ;
+        if(mx>=target)
+        return true;
+        return false;
+    }
 
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int low = 1, high = nums.size(), mn = 0;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (windowfind(mid, nums, target)) {
+                high = mid-1;
+                mn = mid;
+            } else low = mid + 1;
+        }
+        return mn;
     }
 };
