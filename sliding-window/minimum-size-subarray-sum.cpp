12d8b14e1b3c4580012dcb311def93c1361f5663
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-08-22 10:47:09 +0530

    LC-minimum-size-subarray-sum [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/minimum-size-subarray-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        //int i =0 ;j =i+1;
+        int n= nums.size();
+        int count = INT_MAX;
+        for(int i =0 ; i<n;i++){
+            for( int j = i+1;j<n; j++ ){
+                if( nums[ i] + nums[j] == target){
+                    count = min( count , j -i+1 );
+                    i++ ;
+                    
+                }else if ( nums[ i] + nums[j] <target){
+                    j++;
+                }else if(nums[ i] + nums[j] > target){
+                    count = min( count , j -i+1 );
+                    i++; 
+                }
+            }
+        }
+        return count == INT_MAX ? 0 : count ;
+
+    }
+};
