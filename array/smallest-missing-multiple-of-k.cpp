8b53dd2ced78fbf9d19be6b475df716d2421e5a1
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-08-26 00:30:10 +0530

    LC-smallest-missing-multiple-of-k [Runtime: 0 ms; Memory: 24.1 MB]

--- array/smallest-missing-multiple-of-k.cpp
+++ array/smallest-missing-multiple-of-k.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        return (nums[n-1] % k == 0) ? nums[n-1] + k : nums[n-1] + (nums[n-1] % k  );
+        vector<bool> present(101, false);
+
+        for (int num : nums) {
+            present[num] = true;
+        }
+
+        for (int multiple = k; ; multiple += k) {
+            if (multiple > 100 || !present[multiple]) {
+                return multiple;
+            }
+        }
+        return -1;
     }
 };
