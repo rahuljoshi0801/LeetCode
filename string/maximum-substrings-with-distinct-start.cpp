95eeb873ff954c5cd821a9e90a6c9ee2a9b68f63
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-08-27 01:37:24 +0530

    LC-maximum-substrings-with-distinct-start [Runtime: N/A; Memory: N/A]

--- string/maximum-substrings-with-distinct-start.cpp
+++ string/maximum-substrings-with-distinct-start.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> taken(26, 0);
         int  count = 0;
         for (char &ch : s){
-            if ( taken[ch - 'a']= 0 ){
+            if ( taken[ch - 'a']== 0 ){
                 count++;
             }
         }
