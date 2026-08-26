e0f9a9f159791602fa4412c60ba48e72e0a318b8
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-08-27 01:36:57 +0530

    LC-maximum-substrings-with-distinct-start [Runtime: N/A; Memory: N/A]

---
+++ hash-table/maximum-substrings-with-distinct-start.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxDistinct(string s) {
+        vector<int> taken(26, 0);
+        int  count = 0;
+        for (char &ch : s){
+            if ( taken[ch - 'a']= 0 ){
+                 count++;
+            }else continue;
+        }
+        return count ;
+
+    }
+};
