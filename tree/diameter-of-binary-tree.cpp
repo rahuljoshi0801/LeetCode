96b77ac3539189c8abe3ba39da1a54d65418198d
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-09-13 20:02:54 +0530

    LC-diameter-of-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ tree/diameter-of-binary-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int diameter = 0 ;
+    int height(TreeNode* root){
+        if (!root) {
+            return 0;
+        }
+        int l = height(root->left , diameter); 
+        int r = height(root->right , diameter );
+        diameter = max(diameter , l + r ) ; 
+
+        return  1 + max(l ,r); 
+        
+        
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        return diameter;
+        
+    }
+};
