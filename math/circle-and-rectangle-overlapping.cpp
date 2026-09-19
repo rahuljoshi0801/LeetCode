a776b62467bd5ce91616c5523f17173a81731105
Rahul Joshi <94026659+rahuljoshi0801@users.noreply.github.com>
2026-09-19 15:55:17 +0530

    LC-circle-and-rectangle-overlapping [Runtime: N/A; Memory: N/A]

---
+++ math/circle-and-rectangle-overlapping.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int xi , yi ;
+        if(x1 > xCenter ) xi = x1;
+        else if( x2 < xCenter) xi = x2 ;
+        else xi = xCenter ; 
+
+        if(y1 > yCenter ) yi = y1;
+        else if( y2 < yCenter) yi = y2 ;
+        else yi = yCenter ; 
+
+        if(radius >= (pow( 2, xi - xCenter ))*(pow( 2, yi - yCenter ))) return true;
+
+        return false ;
+    }
+};
